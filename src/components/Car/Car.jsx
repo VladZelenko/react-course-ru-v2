66ba9a52c980093ac2241498ca91f9d817311b2c
Vlad6555 <Vlad6555@ukr.net>
2019-05-17 11:08:39 +0300

    radium package style

--- src/components/Car/Car.jsx
+++ src/components/Car/Car.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import classes from './Car.module.css';
+import Radium from 'radium';
 
 class Car extends Component {
     render() {
+        const style = {
+            ':hover': {
+                border: '1px solid #fff',
+                boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+                cursor: 'pointer',
+            }
+        }
         const inputClasses = [classes.input]
+
         if (this.props.name !== '') {
             inputClasses.push(classes.green)
         } else {
@@ -13,8 +22,9 @@ class Car extends Component {
         if (this.props.name.length > 4) {
             inputClasses.push(classes.bold)
         }
+
         return (
-            <div className={classes.Car}>
+            <div className={classes.Car} style={style}>
                 <p>Car: {this.props.name}</p>
                 <p>Year {this.props.year}</p>
                 <input
@@ -29,4 +39,4 @@ class Car extends Component {
     }
 }
 
-export default Car;
+export default Radium(Car);
